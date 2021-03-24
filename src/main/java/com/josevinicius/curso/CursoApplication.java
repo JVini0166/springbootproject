3da7a7d7b084ef6cfdc8a500da8e53a064ea709f
JVini0166 <joseviniciusgta@gmail.com>
2021-03-24 19:30:13 -0300

    Started SpringBoot Project

---
+++ src/main/java/com/josevinicius/curso/CursoApplication.java
@@ -0,0 +1,14 @@
+package com.josevinicius.curso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoApplication {
+
+	public static void main(String[] args) {
+		System.out.println("teste");
+		SpringApplication.run(CursoApplication.class, args);
+	}
+
+}
