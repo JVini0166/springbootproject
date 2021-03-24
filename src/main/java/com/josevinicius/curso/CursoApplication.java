02f3778464ba3ac155de7e6b58af9c34fc8e12d0
JVini0166 <joseviniciusgta@gmail.com>
2021-03-24 19:51:36 -0300

    configured REST, testing REST

--- src/main/java/com/josevinicius/curso/CursoApplication.java
+++ src/main/java/com/josevinicius/curso/CursoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CursoApplication {
 
 	public static void main(String[] args) {
-		System.out.println("teste");
+		
 		SpringApplication.run(CursoApplication.class, args);
 	}
 
